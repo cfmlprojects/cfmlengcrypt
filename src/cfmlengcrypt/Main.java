8dbb8d15984ded62b05361044a68032708bb6237
denuno <valliantster@gmail.com>
2015-05-31 20:24:39 -0600

    usage

--- src/cfmlengcrypt/Main.java
+++ src/cfmlengcrypt/Main.java
@@ -8,12 +8,15 @@ public class Main {
 				RailoEncrypt.encrypt(args[1]);
 			} else if (args[0].toLowerCase().equals("sha")) {
 				ShaHash.encrypt(args[1]);
-			} else {
+			} else if (args[0].toLowerCase().equals("acf")) {
 				ACFDSNEncrypt.encrypt(args[1]);
+			} else {
+				throw new RuntimeException("Unknown type: " + args[0]);
 			}
 
 		} else {
 			System.out.println("Usage:");
+			System.out.println("java -jar cfmlengcrypt lucee somepassword");
 			System.out.println("java -jar cfmlengcrypt railo somepassword");
 			System.out.println("java -jar cfmlengcrypt acf somepassword");
 			System.out.println("java -jar cfmlengcrypt sha somepassword");
